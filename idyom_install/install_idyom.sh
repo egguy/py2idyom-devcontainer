36ae3444b8859703e5df8371420b68eec354c278
egguy <etienne.guilluy@gmail.com>
2023-11-27 09:40:27 +0000

    Cleanup of the install script + update to 1.7.1
    - Removed unused script
    - Updated to idyom 1.7.1
    - Simplified installation

--- idyom_install/install_idyom.sh
+++ idyom_install/install_idyom.sh
@@ -1,28 +1,44 @@
 #!/bin/sh
 
+set -e
 
-SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-USR_HOME_PATH=$HOME
+IDYOM_VERSION="1.7.1"
 
 
+cd "$HOME"
+mkdir -p idyom/ idyom/db/ idyom/data/cache/ idyom/data/models/ idyom/data/resampling/ quicklisp/
 
-echo ";;; Load CLSQL by default" > ${SCRIPTPATH}/install_pt3.lisp
-echo "(ql:quickload \"clsql\")" >> ${SCRIPTPATH}/install_pt3.lisp
+cd quicklisp && curl -O https://beta.quicklisp.org/quicklisp.lisp  # && sbcl --load quicklisp.lisp --eval '(quicklisp-quickstart:install)' --eval '(ql:add-to-init-file)' --eval '(ql:quickload "quicklisp-slime-helper")' --eval '(quit)' 
 
-IDYOM_ROOT_PATH="$USR_HOME_PATH/idyom/"
-IDYOM_DB_PATH="$USR_HOME_PATH/idyom/db/database.sqlite"
+INSTAL_SCRIPT='(load (merge-pathnames "quicklisp/quicklisp.lisp" (user-homedir-pathname)))
+(quicklisp-quickstart:install :path (merge-pathnames "quicklisp/" (user-homedir-pathname)))
+(ql-util:without-prompting  (ql:add-to-init-file))
+(ql:quickload "quicklisp-slime-helper")
+(SB-EXT:EXIT)'
+echo "$INSTAL_SCRIPT" > sbcl_instal.lisp
+sbcl --non-interactive  --load sbcl_instal.lisp
 
-echo ";;; IDyOM" >> ${SCRIPTPATH}/install_pt3.lisp
+# Define the content to be written
+content="(load (expand-file-name \"~/quicklisp/slime-helper.el\"))
+;; Replace \"sbcl\" with the path to your implementation
+(setq inferior-lisp-program \"sbcl\")"
+
+# Write the content to the ~/.emacs file
+echo "$content" > ~/.emacs
+
+
+# IDYOM_ROOT_PATH="$HOME/idyom/"
+# IDYOM_DB_PATH="$HOME/idyom/db/database.sqlite"
+echo ";;; Load CLSQL by default" >> ~/.sbclrc
+echo "(ql:quickload \"clsql\")" >> ~/.sbclrc
+echo ";;; IDyOM" >> ~/.sbclrc
 echo "(defun start-idyom ()
-   (defvar *idyom-root* \"$IDYOM_ROOT_PATH\")
+   (defvar *idyom-root* \"$HOME\")
+   (defvar *idyom-message-detail-level* 1)
    (ql:quickload \"idyom\")
-   (clsql:connect '(\"$IDYOM_DB_PATH\") :if-exists :old :database-type :sqlite3))" >> ${SCRIPTPATH}/install_pt3.lisp
+   (clsql:connect '(\"$HOME\") :if-exists :old :database-type :sqlite3))" >> ~/.sbclrc
 
+cd "$HOME"
+mkdir -p quicklisp/local-projects/ && curl -L https://github.com/mtpearce/idyom/archive/refs/tags/v${IDYOM_VERSION}.tar.gz > idyom.tar.gz && tar -xzf idyom.tar.gz -C quicklisp/local-projects/ && rm idyom.tar.gz
 
-INSTALL3_PATH=${SCRIPTPATH}/install_pt3.lisp
-cat $INSTALL3_PATH >> ~/.sbclrc
-
-
-INSTALL4_PATH=${SCRIPTPATH}/install_pt4.lisp
-cd ~ || exit
-sbcl --load $INSTALL4_PATH
+sbcl --eval '(start-idyom)' --eval '(idyom-db:initialise-database)' --eval '(SB-EXT:EXIT)'
--- idyom_install/install_pt4.lisp
+++
@@ -1,4 +0,0 @@
-;;; initialize db
-(start-idyom)
-(idyom-db:initialise-database)
-(SB-EXT:EXIT)
